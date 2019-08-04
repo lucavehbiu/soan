bea62f350bc9a75109e85c63f7e10f109ff9038a
LucaV <lucavehbiu96@gmail.com>
2019-08-04 09:17:41 +0200

    change pre-req

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ setup(
     packages=['soan'],
     description='Used to analyze whatsapp data',
     long_description=open('README.txt').read(),
-    install_requires=['numpy', 'PIL', 'scipy', 'sklearn', 'regex', 're', 'emoji', 'seaborn',
-                     'datetime', 'itertools', 'pandas', 'operator', 'requests', 'mpl_toolkits', 
-                      'wordcloud', 'pattern', 'palettable'],
+    install_requires=['numpy', 'pillow', 'scipy', 'sklearn', 'regex', 'emoji', 'seaborn',
+                     'datetime', 'pandas', 'operator', 'requests',
+                      'wordcloud', 'palettable'],
     version='0.1',
     license='MIT',
 )
